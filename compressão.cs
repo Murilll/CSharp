8dc1f48fd818b176b4a5decda2c116e931e087da
Murilo Sertorio <alisonhenrique@msn.com>
2022-10-14 11:19:26 -0300

    Tempo da compressão

--- compressão.cs
+++ compressão.cs
@@ -3,6 +3,14 @@ using System;
 int[] arr = new int[]
 {192, 255};
 
+void compressAndDecompress(byte[] bytes){  // Contar o tempo da compressão
+    var start = Datetime.Now;
+    var compressed = compress(bytes);
+    bytes = decompress(compressed);
+    var end = DateTime.Now;
+    MessageBox.Show((end - start).TotalMilliseconds.ToString() + " ms");
+}
+
 for (int i = 0; i < arr.Length; i += 2){
     int x = arr[i];
     int x1 = arr[i+1];
@@ -13,3 +21,4 @@ for (int i = 0; i < arr.Length; i += 2){
 
     Console.WriteLine($"Compressão: {compress}");
 }
+
